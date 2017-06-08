0cb4296ad933fcce554090f580568e2cf09449c2
hiruke <ronneysantiago@hotmail.com>
2017-06-07 21:02:50 -0300

    aposidh

--- FirmDAL/FirmDAL/DBCon.cs
+++ FirmDAL/FirmDAL/DBCon.cs
@@ -10,7 +10,7 @@ namespace FirmDAL
 			public static class DBCon
 			{
 						static private SqlConnection connection = null;
-						static private string server = "187.33.48.115";
+						static private string server = "hiruke.ddns.net";
 						static private string database = "firm";
 						static private string port = "14333";
 						static private string user = "firm";
